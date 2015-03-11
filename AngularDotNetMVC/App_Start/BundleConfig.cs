00ca5675ec17bb1aafdc96a4cc1ca75471b2cc01
LeonM234 <LeonM234@yahoo.com>
2015-03-11 15:24:13 -0700

    Adding new bundles and injecting into Index.cshtml

--- AngularDotNetMVC/App_Start/BundleConfig.cs
+++ AngularDotNetMVC/App_Start/BundleConfig.cs
@@ -9,6 +9,10 @@ namespace AngularDotNetMVC
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/site.css"));
 
+            bundles.Add(new ScriptBundle("~/bundles/AngularDotNetMVC")
+                .IncludeDirectory("~/Scripts/Controllers", "*.js")
+                .Include("~/Scripts/AngularDotNetMVC.js"));
+
             BundleTable.EnableOptimizations = true;
         }
     }
