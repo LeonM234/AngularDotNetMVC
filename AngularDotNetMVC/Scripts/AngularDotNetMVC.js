8b539b5a3d87b975687a45842d0ff338ddf5e519
LeonM234 <LeonM234@yahoo.com>
2015-03-11 19:51:52 -0700

    Add routescontroller along with 3 new views.

--- AngularDotNetMVC/Scripts/AngularDotNetMVC.js
+++ AngularDotNetMVC/Scripts/AngularDotNetMVC.js
@@ -1,3 +1,19 @@
 ﻿var AngularDotNetMVC = angular.module('AngularDotNetMVC', []);
 
 AngularDotNetMVC.controller('LandingPageController', LandingPageController);
+
+var configFunction = function ($routeProvider) {
+    $routeProvider.
+        when('/routeOne', {
+            templateUrl: 'routesDemo/one'
+        })
+        .when('/routeTwo', {
+            templateUrl: 'routesDemo/two'
+        })
+        .when('/routeThree', {
+            templateUrl: 'routesDemo/three'
+        });
+}
+configFunction.$inject = ['$routeProvider'];
+
+AngularDotNetMVC.config(configFunction);
